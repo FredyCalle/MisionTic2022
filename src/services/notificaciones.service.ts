0134531779a04fac980ae0de971015ddfc1b43e6
Fredy Calle <90583287+FredyCalle@users.noreply.github.com>
2021-11-28 20:31:59 -0500

    Update notificaciones.service.ts

--- src/services/notificaciones.service.ts
+++ src/services/notificaciones.service.ts
@@ -10,8 +10,8 @@ export class NotificacionesService {
 
   EnviarNotificacionesPorSMS(SMS:string,Telefono:string):void{
     console.log('probando envio mensajeria por Twilio');
-    const accountSid = 'ACf3cf2705b9f672c67500e993f78b735d'; // Your Account SID from www.twilio.com/console
-    const authToken = '061852acabe5b99701fb7ddaa090cdf7'; // Your Auth Token from www.twilio.com/console //siempre verificar y modificar el Token
+    const accountSid = ''; // Your Account SID from www.twilio.com/console
+    const authToken = ''; // Your Auth Token from www.twilio.com/console //siempre verificar y modificar el Token
     
     const twilio = require('twilio');
     const client = new twilio(accountSid, authToken);
